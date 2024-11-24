5e87501ea9182cb79be05d49a2156b358b55c38b
Your Name <you@example.com>
2024-11-25 04:01:49 +0530

    Added GenericRubixCuber , Excited for the Project :)

---
+++ GenericRubixCube.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Asus on 25-11-2024.
+//
+
+#include "GenericRubixCube.h"
---
+++ GenericRubixCube.h
@@ -0,0 +1,16 @@
+//
+// Created by Asus on 25-11-2024.
+//
+
+#ifndef GENERICRUBIXCUBER_H
+#define GENERICRUBIXCUBER_H
+
+
+
+class GenericRubixCube {
+
+};
+
+
+
+#endif //GENERICRUBIXCUBER_H
